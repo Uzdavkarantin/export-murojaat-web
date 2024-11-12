cc505eff67baf5b2f14edc2722f9af21980215ab
NuriddinovQudrat <qudratjonnuriddinov2603@gmail.com>
2024-11-12 09:53:54 +0500

    libraries

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import eslint from "vite-plugin-eslint";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    eslint({
+      cache: false,
+      include: ["./src/**/*.tsx", "./src/**/*.ts"],
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
