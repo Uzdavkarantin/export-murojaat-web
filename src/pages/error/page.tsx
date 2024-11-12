a238f3d00afecfe21b722179faff6d8e3389a836
NuriddinovQudrat <qudratjonnuriddinov2603@gmail.com>
2024-11-12 11:33:11 +0500

    error page ui

--- src/pages/error/page.tsx
+++ src/pages/error/page.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button";
 import { ChildProps } from "@/types";
 import { QueryErrorResetBoundary } from "@tanstack/react-query";
 import { ErrorBoundary as ClassBoundary } from "react-error-boundary";
+import codeErrorImg from "@/assets/icons/code-error.png";
 
 const ErrorBoundary = ({ children }: ChildProps) => {
   return (
@@ -10,13 +11,11 @@ const ErrorBoundary = ({ children }: ChildProps) => {
         <ClassBoundary
           onReset={reset}
           fallbackRender={({ resetErrorBoundary }) => (
-            <div className="p-10 h-screen items-center justify-center">
-              <h3>Oops! Something went Awry</h3>
-              <p>
-                We’re sorry, but seems there’s been a hiccup on our end. Our team has been notified
-                and is working diligently to resolve this issue.
-              </p>
-              <Button className="w-full max-w-32" onClick={resetErrorBoundary}>
+            <div className="container mx-auto flex h-screen items-center justify-center flex-col space-y-3 p-2 text-center">
+              <img src={codeErrorImg} alt="error" className="w-36" />
+              <h3 className="text-4xl font-semibold text-primary">Oops! Something went wrong!</h3>
+              <p className="text-sm font-semibold">Don't worry, our team is here to help!</p>
+              <Button className="w-full max-w-36" onClick={resetErrorBoundary}>
                 Try again
               </Button>
             </div>
