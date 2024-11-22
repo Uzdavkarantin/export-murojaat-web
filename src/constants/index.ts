7901de22dae5348bc323790636b24550051f636a
NuriddinovQudrat <qudratjonnuriddinov2603@gmail.com>
2024-11-22 12:52:55 +0500

    base url changed

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
-export const domain = "192.168.0.200:8000";
-export const imgBaseURL = `http://${domain}`;
-export const baseURL = `http://${domain}/api/v1`;
+export const domain = "bot.karantin.uz";
+export const imgBaseURL = `https://${domain}`;
+export const baseURL = `https://${domain}/api/v1`;
 export const wsURL = `ws://${domain}/api/ws/`;
