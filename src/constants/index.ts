d100cb4559e0f26898dd865eac46851070416d68
NuriddinovQudrat <qudratjonnuriddinov2603@gmail.com>
2024-11-25 10:23:07 +0500

    ws url changed

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
 export const domain = "bot.karantin.uz";
 export const imgBaseURL = `https://${domain}`;
 export const baseURL = `https://${domain}/api/v1`;
-export const wsURL = `wss://${domain}/api/ws/`;
+export const wsURL = `wss://${domain}/ws/`;
